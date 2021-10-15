3618f5216cf3011de6b682e47456ae489f2acb82
Alice Won <alice.w@accretivetg.com>
2021-10-15 01:17:18 -0700

    added sql file and test data

--- db/data/tasks_data.sql
+++ db/data/tasks_data.sql
@@ -1,9 +1,12 @@
 use sporksdb;
 
-INSERT INTO `tasks` (
-  `task_id`, `item`, `created`, `completed` )
+
+INSERT INTO tasks(item, completed)
 VALUES
-(1, "buy cat food", 2021-10-12 23:15:45.073, 0),
-(2, "grocery shop", 2021-10-12 23:16:24.590, 0),
-(3, "schedule drs apt", 2021-10-12 23:16:51.340, 1),
-(4, "add okrs to upraise", 2021-10-12 23:17:24.167, 0)
+('buy dog food', 0),
+('groceries', 0),
+('water plants', 1),
+('run in a circle', 0),
+('make soup', 0),
+('pay electricity bill', 0),
+('put OKRs into UpRaise', 1);
--- db/table/tasks.sql
+++ db/table/tasks.sql
@@ -5,7 +5,7 @@ use sporksdb;
 CREATE TABLE IF NOT EXISTS `tasks`(
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `item` varchar(255) NOT NULL,
-  `created` datetime NOT NULL, DEFAULT GETUTCDATE(),
+  `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP(),
   `completed` tinyint(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`)
 );
