9798c7d4dc98ccdc39c13203c14a1ad4711f59ed
Alice Won <alice.w@accretivetg.com>
2021-10-15 01:56:31 -0700

    setup react

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+
+ReactDOM.render(
+    <h1>sporks is ready for some code!</h1>,
+    document.getElementById('app-root'),
+)
